afaeec6c2953d7ecd22971abb9f3fef5e365900c
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-09 03:14:48 +0300

    ..

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
+console.log("ANYBODY -> ", import.meta.env.PUBLIC_SANITY_PROJECT_DATASET);
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,10 +11,14 @@ export default defineConfig({
     }),
     sanity({
       //TODO:env variables
-      projectId: "9ebfcrzt",
-      dataset: "production",
+      projectId: import.meta.env.DEV
+        ? import.meta.env.VITE_SANITY_PROJECT_ID
+        : JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_ID),
+      dataset: import.meta.env.DEV
+        ? import.meta.env.VITE_SANITY_PROJECT_DATASET
+        : JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_DATASET),
       // Set useCdn to false if you're building statically.
-      useCdn: true,
+      useCdn: false,
       apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
     }),
   ],
