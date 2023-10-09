0d68d239171ed28a3d44ac7218dfac066eb4289a
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-09 05:50:18 +0300

    Refactor: Implement env variables for CMS config

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
-console.log("ANYBODY -> ", import.meta.env.PUBLIC_SANITY_PROJECT_DATASET);
+import { loadEnv } from "vite";
+const { SANITY_PROJECT_ID, SANITY_PROJECT_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,12 +16,8 @@ export default defineConfig({
     }),
     sanity({
       //TODO:env variables
-      projectId: import.meta.env.DEV
-        ? import.meta.env.VITE_SANITY_PROJECT_ID
-        : JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_ID),
-      dataset: import.meta.env.DEV
-        ? import.meta.env.VITE_SANITY_PROJECT_DATASET
-        : JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_DATASET),
+      projectId: SANITY_PROJECT_ID,
+      dataset: SANITY_PROJECT_DATASET,
       // Set useCdn to false if you're building statically.
       useCdn: false,
       apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
