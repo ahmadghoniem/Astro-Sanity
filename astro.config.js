65d28210913e69f327d9aa3b0fefa5d8f5f067f9
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-09 05:18:48 +0300

    test: env var

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
-console.log("ANYBODY -> ", import.meta.env.DEV);
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,12 +10,14 @@ export default defineConfig({
     }),
     sanity({
       //TODO:env variables
-      projectId: import.meta.env.DEV
-        ? import.meta.env.VITE_SANITY_PROJECT_ID
-        : JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_ID),
-      dataset: import.meta.env.DEV
-        ? import.meta.env.VITE_SANITY_PROJECT_DATASET
-        : JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_DATASET),
+      projectId:
+        import.meta.env.PUBLIC_VERCEL_ENV === "production"
+          ? JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_ID)
+          : import.meta.env.VITE_SANITY_PROJECT_ID,
+      dataset:
+        import.meta.env.PUBLIC_VERCEL_ENV === "production"
+          ? JSON.stringify(import.meta.env.PUBLIC_SANITY_PROJECT_DATASET)
+          : import.meta.env.VITE_SANITY_PROJECT_DATASET,
       // Set useCdn to false if you're building statically.
       useCdn: false,
       apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
