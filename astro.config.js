9f9c9bcc6febd71fc970cc2054d8d276fb94b18a
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-10 06:17:02 +0300

    fix: static rather than serverless

--- astro.config.js
+++ astro.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
 import { loadEnv } from "vite";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 const { SANITY_PROJECT_ID, SANITY_PROJECT_DATASET } = loadEnv(
   process.env.NODE_ENV,
   process.cwd(),
