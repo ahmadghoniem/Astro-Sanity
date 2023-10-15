7b54b7fca03ef808b463a41174c6bc8d1d4b570d
kontb2a22 <ahmedxd.2009@gmail.com>
2023-10-15 21:11:15 +0300

    feat:added new page about & refactored contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
-  darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
