9f2524048d32886b3617ea0eddb329ee4edf9470
kontb2a22 <ahmedxd.2009@gmail.com>
2023-12-18 07:38:43 +0200

    fix: vercel env variables

--- astro.config.js
+++ astro.config.js
@@ -3,14 +3,23 @@ import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
 import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel/static";
-const { SANITY_PROJECT_ID, SANITY_PROJECT_DATASET } = loadEnv(
-  process.env.NODE_ENV,
-  process.cwd(),
-  "",
-);
+const {
+  SANITY_PROJECT_ID,
+  SANITY_PROJECT_DATASET,
+  EMAILJS_PUBLIC_KEY,
+  EMAILJS_SERVICE_ID,
+  EMAILJS_TEMPLATE_ID,
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    define: {
+      "process.env.EMAILJS_PUBLIC_KEY": JSON.stringify(EMAILJS_PUBLIC_KEY),
+      "process.env.EMAILJS_SERVICE_ID": JSON.stringify(EMAILJS_SERVICE_ID),
+      "process.env.EMAILJS_TEMPLATE_ID": JSON.stringify(EMAILJS_TEMPLATE_ID),
+    },
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
